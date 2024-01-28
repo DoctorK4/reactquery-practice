0d1adefa0de3cf7af9f25ec8d546ec9549b6f750
doctork4 <doctor.k1994@gmail.com>
2024-01-28 23:41:40 +0900

    feat: 포스트목록 렌더링

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,10 +1,15 @@
+import { useQuery } from '@tanstack/react-query';
 import Post from './Post';
 import styles from './PostList.module.css';
+import { getPosts } from '../api';
 
 function PostList() {
-  // 여기에 코드를 작성하세요
-
-  const posts = [];
+  const result = useQuery({
+    queryKey: ['posts'],
+    queryFn: getPosts
+  });
+  
+  const posts = [...result.data.results]; 
 
   return (
     <div className={styles.postList}>
